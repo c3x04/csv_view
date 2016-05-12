d94990d70382df72c02b6ce73e278a99a4f3c2c0
c3x04 <akashsharma95@outlook.com>
2016-05-12 14:49:32 +0530

    Sync build to debug electron error on windows

---
+++ build/app.js
@@ -0,0 +1,48 @@
+(function () { 'use strict';
+
+  'use strict';
+
+  var angular = require('angular');
+  require('angular-route');
+  require('angular-material');
+  require('d3');
+  require('angular-nvd3');
+  require('nvd3');
+  var _templateBase = './templates';
+
+  var app = angular.module('app', [require('angular-material-data-table'), 'nvd3', 'ngRoute', 'ngAnimate', 'lfNgMdFileInput', 'ngMaterial']);
+
+  var index = require('./modules/index');
+
+  app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: _templateBase + '/index.html',
+      controller: 'indexController',
+    });
+    $routeProvider.when('/table', {
+      templateUrl: _templateBase + '/table.html',
+      controller: 'tableController',
+    });
+
+    $routeProvider.when('/visualizations', {
+      templateUrl: _templateBase + '/visualizations.html',
+      controller: 'chartController',
+    });
+
+    $routeProvider.otherwise({ redirectTo: '/' });
+
+  }]);
+  app.service('sharedData', function() {
+    var data = [];
+    return {
+      getData: function() {
+        return data;
+      },
+      setData: function(obj) {
+        data = obj;
+      }
+    }
+  });
+
+})();
+//# sourceMappingURL=app.js.map
